0297a8df88cc4032dab66e0a2c1c4f10f93bbe71
Mr2803 <rispoli.mar2803@gmail.com>
2020-02-08 15:09:00 +0100

    update value
    change value for test

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -16,7 +16,7 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        factory(Apartment::class , 50) 
+        factory(Apartment::class , 51) 
         -> make() 
         -> each(function($apartment){
 
