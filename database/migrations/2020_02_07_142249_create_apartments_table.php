2663e7ae5bd5303358be9fa7094d79329877bcd7
leos <leonardo.siri.email@gmail.com>
2020-02-10 22:30:23 +0100

    pass a number instead of a string when calling default

--- database/migrations/2020_02_07_142249_create_apartments_table.php
+++ database/migrations/2020_02_07_142249_create_apartments_table.php
@@ -17,13 +17,13 @@ class CreateApartmentsTable extends Migration
             $table->bigIncrements("id");
             $table->string("description");
             $table->unsignedTinyInteger("rooms");
-            $table->unsignedTinyInteger("beds")->default("1");
-            $table->unsignedTinyInteger("bathrooms")->default("1");
+            $table->unsignedTinyInteger("beds")->default(1);
+            $table->unsignedTinyInteger("bathrooms")->default(1);
             $table->smallInteger("square_feet");
             $table->string("address");
             $table->decimal("lat", 10, 7);
             $table->decimal("lon", 10, 7);
-            $table->integer("views")->default("0");
+            $table->integer("views")->default(0);
             $table->string("poster_img")->nullable();
             $table->timestamps();
         });
