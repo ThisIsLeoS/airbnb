fa80bfaee4591ea9fc3d04b4e5c6c567dd126b45
leos <leonardo.siri.email@gmail.com>
2020-02-10 20:47:10 +0100

    create Apartment request and add validation rules

---
+++ app/Http/Requests/ApartmentRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ApartmentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "description" => "required|string|max:255",
+            "rooms" => "required|integer|gt:0|max:255",
+            "beds" => "required|integer|gt:0|max:255",
+            "bathrooms" => "required|integer|gt:0|max:255",
+            "address" => "required|string|max:255",
+            "lat" => "required|numeric|between:-90,90",
+            "lon" => "required|numeric|between:-180,180",
+            "views" => "required|integer|gte:0",
+            "poster_path" => "string|max:255|nullable"
+        ];
+    }
+}
